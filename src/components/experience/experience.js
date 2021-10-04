83d6d0ccdbbade8365e43cabce70977051f752ab
kalyani joshi <kalyani121@gmail.com>
2021-10-04 17:47:01 -0400

    updated card styling

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -4,14 +4,12 @@ import'./experience.css';
 const Experience = ({ details }) => {
   const { employerName, period, role, techStack } = details;
   return (
-    <div className="empHistory col-6 box-shadow mb-4 mr-4">
+    <div className="col-6 box-shadow mb-4 mr-4">
       <div className="empName card-header">{employerName}</div>
       <div className="card-body">
-        <div className="card-title"> {period} 
-        <div>{role}</div>
-        </div>
-        
-        <h5 className="card-text"> <strong>Technology Stack:</strong> {techStack}</h5>
+        <div className="card-title mb-2"> {period}</div>
+        <div className="card-subtitle mb-2">{role}</div>      
+        <div className="card-text">Technology Stack: {techStack}</div>
       </div>
     </div>
   );
