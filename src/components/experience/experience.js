8dfca690e5377b3fa0a18b1e93e9addec20d1c92
kalyani joshi <kalyani121@gmail.com>
2021-10-04 09:40:20 -0400

    fixed the card body issue in experience component

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Experience = ({ expDetails }) => {
-  const { employerName, period, role, techStack } = expDetails;
+const Experience = ({ details }) => {
+  const { employerName, period, role, techStack } = details;
   return (
     <div className="col-6">
-      <div className="card-header"></div>
-      <div className="card-body">
-        <h5 className="card-title"> </h5>
-        <p className="card-text"></p>
+      <div className="card-header bg-secondary box-shadow">{employerName}</div>
+      <div className="card-body bg-warning">
+        <div className="card-title"> {period} </div>
+        <div>{role}</div>
+        <h5 className="card-text">Technology Stack: {techStack}</h5>
       </div>
     </div>
   );
