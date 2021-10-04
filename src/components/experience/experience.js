7a248acc5a816cd1fe23e9cb05bcf3042a859990
kalyani joshi <kalyani121@gmail.com>
2021-10-04 17:33:22 -0400

    updated card styling

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import'./experience.css';
 
 const Experience = ({ details }) => {
   const { employerName, period, role, techStack } = details;
   return (
-    <div className="col-6">
-      <div className="card-header bg-secondary box-shadow">{employerName}</div>
-      <div className="card-body bg-warning">
-        <div className="card-title"> {period} </div>
+    <div className="empHistory col-6 box-shadow mb-4 mr-4">
+      <div className="empName card-header">{employerName}</div>
+      <div className="card-body">
+        <div className="card-title"> {period} 
         <div>{role}</div>
-        <h5 className="card-text">Technology Stack: {techStack}</h5>
+        </div>
+        
+        <h5 className="card-text"> <strong>Technology Stack:</strong> {techStack}</h5>
       </div>
     </div>
   );
--- src/components/experience/experienceList.js
+++ src/components/experience/experienceList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Experience from './experience';
+import './experience.css';
 
 const ExperienceList = () => {
   const experienceDetails = [
@@ -18,14 +19,13 @@ const ExperienceList = () => {
         Selenium,
         Jira,
         Git,
-        GitHub`
+        GitHub`,
     },
     {
       employerName: 'L&T Infotech (LTI), PA, USA and Mumbai, India',
       period: 'Nov 09- Dec 14',
       role: 'Business Analyst/ QA Lead',
-      techStack: 
-        `Guidewire Claims Center,
+      techStack: `Guidewire Claims Center,
         Guidewire Policy Center,
         JIRA,
         HP ALM,
@@ -34,13 +34,13 @@ const ExperienceList = () => {
         SVN,
         Java,
         Gosu,
-        SQL Server`
+        SQL Server`,
     },
     {
       employerName: 'Auditime Information Systems (I) Ltd., Mumbai, India',
       period: 'Jul 07 – Nov 09',
       role: 'QA Team Lead',
-      techStack: `IIMS, ASP.NET, Oracle 9i`
+      techStack: `IIMS, ASP.NET, Oracle 9i`,
     },
     {
       employerName: 'Eclerx, Mumbai, India',
@@ -49,26 +49,45 @@ const ExperienceList = () => {
       techStack: `Dell Horizon,
         SharePoint,
         HP Quality Center,
-        MS Excel`
+        MS Excel`,
     },
     {
       employerName: 'Dyna Hi-Tech Power Systems Ltd., Mumbai, India',
       period: 'Apr 05 – Oct 06',
       role: 'QA Tester',
-      techStack: `MS Excel`
+      techStack: `MS Excel`,
     },
   ];
 
   return (
-    <div className="experience p-4" id="experience">
-      <h1>Work Experience</h1>
-      <p>Here's my experience summary.</p>
-      
+    <div className="p-4" id="experience">
+      <div className="experience">Work Experience</div>
+      <div className="mb-2">
+        A highly competent and results oriented IT Professional with over
+        Thirteen (13) years of experience in diverse roles: Leadership, Business
+        Analysis and Software testing.{' '}
+      </div>
+
+      <div className="mb-2">
+        Quality focused dynamic leader with ability to motivate and lead
+        diverse, global cross-functional teams in fulfillment of corporate
+        productivity and bottom-line objectives. Proven ability to bridge
+        technology and business goals to provide productive solutions, improve
+        revenue, margins, workplace productivity.
+      </div>
+
+      <div className="mb-2">
+        Detail-focused, result-oriented IT professional in Information
+        technology as a Scrum Master, Business Analyst, and Quality Analyst in
+        high volume, intense customer focused service industries: e-Commerce,
+        Retail, Insurance, Finance and Media – Entertainment.
+      </div>
+
       <div className="row" id="experience-lists">
         {experienceDetails.map((e, i) => (
           <Experience key={i} details={e}></Experience>
         ))}
-        </div>
+      </div>
     </div>
   );
 };
